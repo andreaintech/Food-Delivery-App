8f47f0c9575e43c966eb825df1cd88101e6fec31
adasilvapdev <adasilvapdev@gmail.com>
2021-06-14 09:55:40 -0400

    Added the restaurant list on home screen

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -22,10 +22,10 @@ interface CategoryProps {
     icon: string
 }
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }: any) => {
 
     const [categories, setCategories] = useState(DATA.categoryData)
-    const [selectedCategory, setSelectedCategory] = useState(null)
+    const [selectedCategory, setSelectedCategory] = useState<any>(null)
     const [restaurants, setRestaurants] = useState(DATA.restaurantData)
     const [currentLocation, setCurrentLocation] = useState(DATA.initialCurrentLocation)
 
@@ -88,6 +88,15 @@ const HomeScreen = () => {
 
         setRestaurants(restaurantList)
         setSelectedCategory(category)
+    }
+
+    const getCategoryNameById = (id: number) => {
+        let categoryName = ""
+        let category = categories.filter(categoryItem => categoryItem.id == id)
+        if (category.length > 0)
+            categoryName = category[0].name
+
+        return categoryName
     }
 
     const renderMainCategories = () => {
@@ -154,10 +163,138 @@ const HomeScreen = () => {
             </View>
         )
     }
+
+    const renderRestaurantList = () => {
+        const renderItem = ({ item }: any) => {
+            return (
+                <TouchableOpacity
+                    style={{
+                        marginBottom: SIZES.padding * 2,
+                    }}
+                    onPress={() => navigation.navigate("Restaurant", {
+                        item,
+                        currentLocation
+                    })}
+                >
+                    <View>
+                        <Image
+                            source={item.photo}
+                            resizeMode="cover"
+                            style={{
+                                width: '100%',
+                                height: 200,
+                                borderRadius: SIZES.radius,
+                            }}
+                        />
+                        <View
+                            style={{
+                                position: "absolute",
+                                bottom: 0,
+                                height: 50,
+                                width: SIZES.width * 0.35,
+                                backgroundColor: COLORS.white,
+                                borderTopRightRadius: SIZES.radius,
+                                borderBottomLeftRadius: SIZES.radius,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                ...styles.shadow
+                            }}
+                        >
+                            <Text style={{ ...FONTS.h4 }}>{item.duration}</Text>
+                        </View>
+                    </View>
+
+
+                    <Text style={{
+                        ...FONTS.body2
+                    }}
+                    >
+                        {item.name}
+                    </Text>
+
+                    <View
+                        style={{
+                            marginTop: SIZES.padding,
+                            flexDirection: 'row',
+                        }}
+                    >
+                        {/* Rating */}
+                        <Image
+                            source={ICONS.star}
+                            style={{
+                                height: 20,
+                                width: 20,
+                                tintColor: COLORS.primary,
+                                marginRight: 10
+                            }}
+                        />
+                        <Text style={{
+                            ...FONTS.body3
+                        }}>
+                            {item.rating}
+                        </Text>
+
+                        {/* Categories Label */}
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                marginLeft: 10,
+                            }}
+                        >
+                            {item.categories.map((categoryId: number) => {
+                                return (
+                                    <View
+                                        style={{
+                                            flexDirection: 'row',
+                                        }}
+                                        key={categoryId}
+                                    >
+                                        <Text>{getCategoryNameById(categoryId)}</Text>
+                                        <Text style={{
+                                            ...FONTS.h3,
+                                            color: COLORS.darkgray
+                                        }}>
+                                            {` - `}
+                                        </Text>
+                                    </View>
+                                )
+                            })}
+
+                            {/* Prices */}
+                            {[1, 2, 3].map((priceRating) => (
+                                <Text
+                                    style={{
+                                        ...FONTS.body3,
+                                        color: (priceRating <= item.priceRating) ? COLORS.primary : COLORS.darkgray
+                                    }}
+                                    key={priceRating}
+                                >
+                                    $</Text>
+                            ))}
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            )
+        }
+
+        return (
+            <FlatList
+                data={restaurants}
+                keyExtractor={item => `${item.id}`}
+                renderItem={renderItem}
+                contentContainerStyle={{
+                    paddingHorizontal: SIZES.padding * 2,
+                    paddingBottom: 30,
+                }}
+            />
+        )
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             {renderHeader()}
             {renderMainCategories()}
+            {renderRestaurantList()}
         </SafeAreaView>
     )
 }
